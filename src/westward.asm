5cb69f5c5db8cacd961612cfc1e43fc90255560d
Samuel Cantrell <samuelcantrell@gmail.com>
2020-05-23 21:26:36 -0700

    Comment out function reading player 2's controller
    We're not utilizing player 2 as far as I'm aware of, so I've commented
    out the code relating to him.

--- src/westward.asm
+++ src/westward.asm
@@ -12,7 +12,7 @@
 ; initialize variables here
 gamestate	.rs 1		; current game state
 buttons1    .rs 1
-buttons2    .rs 1
+;buttons2    .rs 1
 spritemem   .rs 1
 textxpos    .rs 1
 textypos	.rs 1
@@ -145,7 +145,7 @@ NMI:
 
 
   JSR ReadController1  ;;get the current button data for player 1
-  JSR ReadController2  ;;get the current button data for player 2
+  ;JSR ReadController2  ;;get the current button data for player 2
   
 GameEngine:  
   LDA gamestate
@@ -334,20 +334,22 @@ ReadController1Loop:
   DEX
   BNE ReadController1Loop
   RTS
-  
-ReadController2:
-  LDA #$01
-  STA $4016
-  LDA #$00
-  STA $4016
-  LDX #$08
-ReadController2Loop:
-  LDA $4017
-  LSR A            ; bit0 -> Carry
-  ROL buttons2     ; bit0 <- Carry
-  DEX
-  BNE ReadController2Loop
-  RTS  
+
+; As far as I'm aware of, not planning to utilize controller 2, so we can just
+; comment this out. 
+;ReadController2:
+;  LDA #$01
+;  STA $4016
+;  LDA #$00
+;  STA $4016
+;  LDX #$08
+;ReadController2Loop:
+;  LDA $4017
+;  LSR A            ; bit0 -> Carry
+;  ROL buttons2     ; bit0 <- Carry
+;  DEX
+;  BNE ReadController2Loop
+;  RTS  
   
   
     
