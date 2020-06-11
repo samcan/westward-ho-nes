f8e7a6418ba3753f3689e82c15c93e17df13ece1
Samuel Cantrell <samuelcantrell@gmail.com>
2020-06-11 16:44:49 -0700

    Add RLE-compression steps to Python script to auto-regenerate from assets

--- compile.bat
+++ compile.bat
@@ -1,5 +1,10 @@
 @echo off
 echo Cleaning old files...
 call clean.bat
+echo RLE-compressing background files...
+py .\util\compress_rle.py --input .\src\bg_blank_traveling_screen.bin --output .\src\bg_blank_traveling_screen_rle.bin
+py .\util\compress_rle.py --input .\src\bg_instruction_screen.bin --output .\src\bg_instruction_screen_rle.bin
+py .\util\compress_rle.py --input .\src\bg_sprite0_traveling_screen.bin --output .\src\bg_sprite0_traveling_screen_rle.bin
+py .\util\compress_rle.py --input .\src\bg_title_screen.bin --output .\src\bg_title_screen_rle.bin
 echo Compiling NES file...
 asm6f_64.exe -L -c -m src\westward.asm src\westward.nes
