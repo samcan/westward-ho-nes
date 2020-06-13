69c197b8126fe9f8cd6875b4e72f8e169f6d0351
Samuel Cantrell <samuelcantrell@gmail.com>
2020-06-13 15:54:44 -0700

    Fixes issue with blank lines not working correctly

--- compile.bat
+++ compile.bat
@@ -9,13 +9,13 @@ echo ###################################
 py .\util\compress_rle.py --input .\src\bg_blank_traveling_screen.bin --output .\src\bg_blank_traveling_screen_rle.bin
 py .\util\compress_rle.py --input .\src\bg_instruction_screen.bin --output .\src\bg_instruction_screen_rle.bin
 py .\util\compress_rle.py --input .\src\bg_sprite0_traveling_screen.bin --output .\src\bg_sprite0_traveling_screen_rle.bin
-echo
+echo.
 echo # Compressing title screen...
-echo
+echo.
 py .\util\compress_rle.py --input .\src\bg_title_screen.bin --output .\src\bg_title_screen_rle.bin
-echo
+echo.
 echo # Compressing landmark screens...
-echo 
+echo. 
 py .\util\compress_rle.py --input .\src\bg_landmark_kansas_river.bin --output .\src\bg_landmark_kansas_river_rle.bin
 py .\util\compress_rle.py --input .\src\bg_landmark_big_blue_river.bin --output .\src\bg_landmark_big_blue_river_rle.bin
 py .\util\compress_rle.py --input .\src\bg_landmark_fort_kearney.bin --output .\src\bg_landmark_fort_kearney_rle.bin
