50bcc215cb5c639caf602374262d6e7ac5433551
Samuel Cantrell <samuelcantrell@gmail.com>
2020-07-18 08:33:23 -0700

    Found one-liner on Tech Republic which does RLE-encoding

--- util/compress_rle.py
+++ util/compress_rle.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from itertools import *
 
 MAX_BYTE_COUNT = 255
 
@@ -9,72 +10,70 @@ def main(input_file, output_file):
     if os.path.isfile(output_file):
         os.remove(output_file)
 
+    run = False
     with open(input_file, 'rb') as f:
-        bytes_read = f.read()
-        
-        prev_byte = ''
-        count = 0
-        for byte in bytes_read:
-            if prev_byte == '':
-                prev_byte = byte
-                count = 1
-            elif byte == prev_byte:
-                count += 1
-            
-            # I may modify this at some point to use negative
-            # numbers to represent a list of bytes which should
-            # be copied as-is. For example, a count of -6 would mean
-            # that the next six bytes should be copied as-is.
-            #
-            # I could convert a negative number n to an 8-bit two's complement
-            # number for conversion to hex by doing the following per the Wikipedia
-            # article:
-            #
-            # = 2^8 - n
-            #
-            # For n = -5
-            # = 2^8 - 5
-            # = 251
-            #
-            # This means I would need to limit the following count to 127
-            if count == MAX_BYTE_COUNT:
-                write_byte(output_file, count, prev_byte)
-                prev_byte = byte
-                count = 0
-                
-            if byte != prev_byte:
-                write_byte(output_file, count, prev_byte)
-                
-                #print('setting prev_byte to',byte)
-                prev_byte = byte
-                count = 1
-                
-        
-        # write final byte to file
-        write_byte(output_file, count, prev_byte)
-        
-        # tack on terminating #$00 bytes
-        write_byte(output_file, 0, 0)
-    
+        bytes_read_int = f.read()
+        rle_bytes_int = rle_encode(bytes_read_int)
+        rle_bytes_int = break_apart_too_big_counts(rle_bytes_int)
+        rle_bytes_hex = convert_int_to_hex(rle_bytes_int)
+        write_bytes(output_file, rle_bytes_hex)
+
     # print file compression info
     input_file_num_bytes = os.stat(input_file).st_size
     output_file_num_bytes = os.stat(output_file).st_size
-    
+
     print('Input file size (bytes):', input_file_num_bytes)
     print('Output file size (bytes):', output_file_num_bytes)
     compression_pct = 1 - (output_file_num_bytes / input_file_num_bytes)
     print(f"Compression (%): {compression_pct * 100:.1f}")
     print()
 
-def write_byte(output, count, byte):
-    #print('writing',count,'of',bytes([byte]))
+# see https://www.techrepublic.com/article/run-length-encoding-in-python/
+def rle_encode(l):
+    return [(len(list(group)),name) for name, group in groupby(l)]
+
+def break_apart_too_big_counts(l):
+    # break apart any tuples that have a count greater than MAX_BYTE_COUNT
+    new_l = list()
+    for x in range(len(l)):
+        count, byte_int = l[x]
+
+        multiples = count // MAX_BYTE_COUNT
+        remainder = count % MAX_BYTE_COUNT
+
+        new_count = 0
+        if multiples == 0:
+            new_count = count
+        else:
+            new_count = MAX_BYTE_COUNT
+
+        for multiple in range(multiples):
+            new_l.append((new_count, byte_int))
+
+        if remainder != 0:
+            new_l.append((remainder, byte_int))
+
+        x += 1
+    return (new_l)
+
+def convert_int_to_hex(l_ints):
+    l_hexs = list()
+    for tuple_int in l_ints:
+        a, b = tuple_int
+        l_hexs.append(bytes([a]))
+        l_hexs.append(bytes([b]))
+    return l_hexs
+
+
+def write_bytes(output, byteslist):
     with open(output, 'ba') as g:
-        g.write(bytes([count]))
-        g.write(bytes([byte]))
+        for byte in byteslist:
+            g.write(byte)
 
-parser = argparse.ArgumentParser(description='Take bin file and RLE encode it.')
-parser.add_argument('--input', required=True, type=str, help='Input bin file')
-parser.add_argument('--output', required=True, type=str, help='Output bin file')
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Take bin file and RLE encode it.')
+    parser.add_argument('--input', required=True, type=str, help='Input bin file')
+    parser.add_argument('--output', required=True, type=str, help='Output bin file')
+    args = parser.parse_args()
 
-main(args.input, args.output)
+    main(args.input, args.output)
